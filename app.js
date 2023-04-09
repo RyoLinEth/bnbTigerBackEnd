8a2b44d8f18c5e165e606c03e605b4a4e58ba926
RyoLinEth <ryolindev@gmail.com>
2023-04-09 23:50:05 +0800

    Init

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3005;
+
+const path = require('path');
+
+const jsonsDir = path.join(__dirname, 'bnbTigerNFTJson');
+
+app.get('/', (req, res) => {
+    res.send('API Is Working!!');
+});
+
+// Serve static files from the jsons directory
+app.use(express.static(jsonsDir));
+
+// GET /json/:filename - Get a specific JSON file by filename
+app.get('/json/:filename', (req, res) => {
+  const filename = req.params.filename;
+  const filepath = path.join(jsonsDir, filename);
+  res.sendFile(filepath);
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
